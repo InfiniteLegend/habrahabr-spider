146aa85f23cf394202f817c10e46f60823776f44
Stanislav Shelekhov <stanislav.shelekhov@hotmail.com>
2016-09-08 16:10:02 +0300

    Created habraitem

--- habra_spider/habra_spider/items.py
+++ habra_spider/habra_spider/items.py
@@ -9,6 +9,22 @@ import scrapy
 
 
 class HabraItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    """ Collecting content, tags, hubs, comments, author info from habrahabr articles
+
+    """
+
+    article_date        = scrapy.Field()
+    article_category    = scrapy.Field()
+    article_title       = scrapy.Field()
+    article_hubs        = scrapy.Field()
+    article_content     = scrapy.Field()
+    article_tags        - scrapy.Field()
+
+    author_name             = scrapy.Field()
+    author_rating           = scrapy.Field()
+    author_karma            = scrapy.Field()
+    author_specialization   = scrapy.Field()
+
+    comment_date    = scrapy.Field()
+    comment_author  = scrapy.Field()
+    comment_content = scrapy.Field()
